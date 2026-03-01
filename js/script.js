df1d5dded9b24b03a044debe5541ba9baa86f853
Mettevebert <mettevebert@gmail.com>
2026-03-01 20:01:17 +0100

    second commit
    finished task (got help from the class we had on it).

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
+// OPGAVE 2: strict mode skal komme som første linje i scriptet
+"use strict";
 
 // Funktion erklæring
 function updateBackground() {
-const width = window.innerWidth;
-    if (width < 640){
+    const width = window.innerWidth;
+
+    // Eksisterende betingelser (uændrede)
+    if (width < 640) {
         document.body.style.background = "purple";
     }
-    else if (width < 760){
+    else if (width < 760) {
         document.body.style.background = "yellow";
     }
 
+    // OPGAVE 3: Hvis bredden er mindre end 900px → blå baggrund
+    else if (width < 900) {
+        document.body.style.background = "blue";
+    }
+
+    // OPGAVE 4: Hvis bredden er mindre end 1100px → rød baggrund
+    else if (width < 1100) {
+        document.body.style.background = "red";
+    }
+
+    // OPGAVE 5: Hvis bredden er mindre end 1300px → grøn baggrund
+    else if (width < 1300) {
+        document.body.style.background = "green";
+    }
+
     else {
-        document.body.style.background = "lightgray"; 
+        document.body.style.background = "lightgray";
     }
 }
+
 // Kør ved load
 updateBackground();
 
 // Kør igen når vinduet ændres
 window.addEventListener("resize", updateBackground);
-
